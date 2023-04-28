2b258434bb61b2fdbfa7fba48f7e04994566a9ae
Jorge Luis Gonzalez Hernandez <jorgegh@delicias.tecnm.mx>
2023-04-28 09:28:01 -0600

    Manejo de RUTAS

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ var express = require('express');
 var app = express();
 
 app.get ('/', function(req,res){
-    res.send('hola mundo');
+    res.send('Una API basica desde Express');
 
 });
+
+app.get ('/saludo', function(req,res){
+    res.send('Hola desde la API ');
+
+});
+
+
+app.get ('/despedida', function(req,res){
+    res.send('Adios desde una API');
+
+});
+
+
 
 app.listen(3000, function(){
     console.log('Aplicacion ejemplo, escuchando el puerto 3000');
